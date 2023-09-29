61f7c46b3e38577467387aa215f0d96f98dfc051
SamuelMallick <sam.mallick.97@gmail.com>
2023-09-29 08:50:36 +0200

    fleet - d inps to decent

--- examples/ACC_fleet/fleet_decent_mld.py
+++ examples/ACC_fleet/fleet_decent_mld.py
@@ -1,3 +1,5 @@
+import sys
+
 import gurobipy as gp
 import numpy as np
 from ACC_env import CarFleet
@@ -5,6 +7,7 @@ from ACC_model import ACC
 from dmpcrl.core.admm import g_map
 from gymnasium import Env
 from gymnasium.wrappers import TimeLimit
+from mpc_gear import MpcGear
 from mpcrl.core.exploration import ExplorationStrategy, NoExploration
 from mpcrl.wrappers.envs import MonitorEpisodes
 from plot_fleet import plot_fleet
@@ -14,10 +17,19 @@ from dmpcpwa.mpc.mpc_mld import MpcMld
 
 np.random.seed(1)
 
-n = 5  # num cars
-N = 3  # controller horizon
-w = 1e4  # slack variable penalty
+n = 2  # num cars
+N = 5  # controller horizon
+COST_2_NORM = True
+DISCRETE_GEARS = True
 
+if len(sys.argv) > 1:
+    n = int(sys.argv[1])
+if len(sys.argv) > 2:
+    N = int(sys.argv[2])
+if len(sys.argv) > 3:
+    COST_2_NORM = bool(sys.argv[3])
+
+w = 1e4  # slack variable penalty
 ep_len = 100  # length of episode (sim len)
 Adj = np.zeros((n, n))  # adjacency matrix
 if n > 1:
@@ -36,28 +48,67 @@ G_map = g_map(Adj)
 acc = ACC(ep_len, N)
 nx_l = acc.nx_l
 nu_l = acc.nu_l
-system = acc.get_pwa_system()
+full_system = acc.get_pwa_system()
+friction_system = acc.get_friction_pwa_system()
 Q_x_l = acc.Q_x_l
 Q_u_l = acc.Q_u_l
 sep = acc.sep
 d_safe = acc.d_safe
 leader_state = acc.get_leader_state()
 
+large_num = 10000  # large number for dumby bounds on vars
+
 
 class LocalMpcMld(MpcMld):
-    def __init__(self, system: dict, N: int) -> None:
+    def __init__(
+        self, system: dict, N: int, leader: bool = False, trailer: bool = False
+    ) -> None:
         super().__init__(system, N)
+        self.setup_cost_and_constraints(self.u, leader, trailer)
 
-        # extra constraints
-        self.s_ahead = self.mpc_model.addMVar(
-            (1, N + 1), lb=0, ub=float("inf"), name="s_ahead"
+    def setup_cost_and_constraints(self, u, leader=False, trailer=False):
+        if COST_2_NORM:
+            cost_func = self.min_2_norm
+        else:
+            cost_func = self.min_1_norm
+
+        # vars for front and back car
+        self.x_front = self.mpc_model.addMVar(
+            (nx_l, N + 1), lb=large_num, ub=large_num, name="x_front"
         )
-        self.s_behind = self.mpc_model.addMVar(
-            (1, N + 1), lb=0, ub=float("inf"), name="s_behind"
+        self.x_back = self.mpc_model.addMVar(
+            (nx_l, N + 1), lb=-large_num, ub=-large_num, name="x_back"
         )
-        self.safety_constraints_ahead = []
-        self.safety_constraints_behind = []
+
+        self.s_front = self.mpc_model.addMVar(
+            (1, N + 1), lb=0, ub=float("inf"), name="s_front"
+        )
+
+        self.s_back = self.mpc_model.addMVar(
+            (1, N + 1), lb=0, ub=float("inf"), name="s_back"
+        )
+
+        # setting these bounds to zero removes the slack var, as leader and trailer
+        # dont have cars in front or behind respectively
+        if leader:
+            self.s_back.ub = 0
+        if trailer:
+            self.s_front.ub = 0
+
+        obj = 0
+        if leader:
+            temp_sep = np.zeros((2, 1))
+        else:
+            temp_sep = sep
+
         for k in range(N):
+            obj += cost_func(self.x[:, [k]] - self.x_front[:, [k]] - temp_sep, Q_x_l)
+            obj += (
+                cost_func(u[:, [k]], Q_u_l)
+                + w * self.s_front[:, k]
+                + w * self.s_back[:, k]
+            )
+
             # accel cnstrs
             self.mpc_model.addConstr(
                 self.x[1, [k + 1]] - self.x[1, [k]] <= acc.a_acc * acc.ts,
@@ -67,32 +118,57 @@ class LocalMpcMld(MpcMld):
                 self.x[1, [k + 1]] - self.x[1, [k]] >= acc.a_dec * acc.ts,
                 name=f"dec_{k}",
             )
-            # safe distance constraints - RHS updated each timestep by coordinator
-            self.safety_constraints_ahead.append(  # for car in front
+
+            # safe distance constraints
+            if not leader:
                 self.mpc_model.addConstr(
-                    self.x[0, [k]] - self.s_ahead[:, [k]] <= float("inf"),
+                    self.x[0, [k]] - self.s_front[:, [k]]
+                    <= self.x_front[0, [k]] - d_safe,
                     name=f"safety_ahead_{k}",
                 )
-            )
 
-            self.safety_constraints_behind.append(  # for car behind
+            if not trailer:
                 self.mpc_model.addConstr(
-                    self.x[0, [k]] + self.s_behind[:, [k]] >= -float("inf"),
+                    self.x[0, [k]] + self.s_back[:, [k]]
+                    >= self.x_back[0, [k]] + d_safe,
                     name=f"safety_behind_{k}",
                 )
-            )
-        self.safety_constraints_ahead.append(
+
+        obj += cost_func(self.x[:, [N]] - self.x_front[:, [N]] - temp_sep, Q_x_l)
+        obj += +w * self.s_front[:, N] + w * self.s_back[:, N]
+
+        if not leader:
             self.mpc_model.addConstr(
-                self.x[0, [N]] - self.s_ahead[:, [N]] <= float("inf"),
+                self.x[0, [N]] - self.s_front[:, [N]] <= self.x_front[0, [N]] - d_safe,
                 name=f"safety_ahead_{N}",
             )
-        )
-        self.safety_constraints_behind.append(  # for car behind
+
+        if not trailer:
             self.mpc_model.addConstr(
-                self.x[0, [N]] + self.s_behind[:, [N]] >= -float("inf"),
-                name=f"safety_behind_{k}",
+                self.x[0, [N]] + self.s_back[:, [N]] >= self.x_back[0, [N]] + d_safe,
+                name=f"safety_behind_{N}",
             )
-        )
+
+        self.mpc_model.setObjective(obj, gp.GRB.MINIMIZE)
+
+    def set_x_front(self, x_front):
+        for k in range(N + 1):
+            self.x_front[:, [k]].lb = x_front[:, [k]]
+            self.x_front[:, [k]].ub = x_front[:, [k]]
+
+    def set_x_back(self, x_back):
+        for k in range(N + 1):
+            self.x_back[:, [k]].lb = x_back[:, [k]]
+            self.x_back[:, [k]].ub = x_back[:, [k]]
+
+
+class LocalMpcGear(LocalMpcMld, MpcGear):
+    def __init__(
+        self, system: dict, N: int, leader: bool = False, trailer: bool = False
+    ) -> None:
+        super().__init__(system, N)
+        self.setup_gears(N, acc)
+        self.setup_cost_and_constraints(self.u_g, leader, trailer)
 
 
 class TrackingDecentMldCoordinator(MldAgent):
@@ -110,7 +186,16 @@ class TrackingDecentMldCoordinator(MldAgent):
         for i in range(self.n):
             xl = state[self.nx_l * i : self.nx_l * (i + 1), :]
             u[i] = self.agents[i].get_control(xl)
-        return np.vstack(u)
+        if DISCRETE_GEARS:
+            # stack the continuous conttrol at the front and the discrete at the back
+            return np.vstack(
+                (
+                    np.vstack([u[i][:nu_l, :] for i in range(n)]),
+                    np.vstack([u[i][nu_l:, :] for i in range(n)]),
+                )
+            )
+        else:
+            return np.vstack(u)
 
     # current state of car to be tracked is observed and propogated forward
     # to be the prediction
@@ -125,64 +210,28 @@ class TrackingDecentMldCoordinator(MldAgent):
     def observe_states(self, env, timestep):
         for i in range(n):
             if i == 0:  # lead car
-                x_pred_ahead = self.extrapolate_position(
+                x_pred_front = self.extrapolate_position(
                     leader_state[0, [timestep]], leader_state[1, [timestep]]
                 )
-                x_pred_behind = self.extrapolate_position(
+                x_pred_back = self.extrapolate_position(
                     env.x[nx_l * (i + 1), :], env.x[nx_l * (i + 1) + 1, :]
                 )
+                self.agents[i].mpc.set_x_front(x_pred_front)
+                self.agents[i].mpc.set_x_back(x_pred_back)
             elif i == n - 1:  # last car
-                x_pred_ahead = self.extrapolate_position(
+                x_pred_front = self.extrapolate_position(
                     env.x[nx_l * (i - 1), :], env.x[nx_l * (i - 1) + 1, :]
                 )
-                x_pred_behind = None, None
+                self.agents[i].mpc.set_x_front(x_pred_front)
             else:
-                x_pred_ahead = self.extrapolate_position(
+                x_pred_front = self.extrapolate_position(
                     env.x[nx_l * (i - 1), :], env.x[nx_l * (i - 1) + 1, :]
                 )
-                x_pred_behind = self.extrapolate_position(
+                x_pred_back = self.extrapolate_position(
                     env.x[nx_l * (i + 1), :], env.x[nx_l * (i + 1) + 1, :]
                 )
-
-            if i != 0:  # add front distance constraint if not front car
-                for k in range(N + 1):
-                    self.agents[i].mpc.safety_constraints_ahead[k].RHS = (
-                        x_pred_ahead[0, [k]] - d_safe
-                    )
-            if i != n - 1:  # add beack distance constraint if not rear car
-                for k in range(N + 1):
-                    self.agents[i].mpc.safety_constraints_behind[k].RHS = (
-                        x_pred_behind[0, [k]] + d_safe
-                    )
-
-            # set costs
-            if i == 0:
-                desired_sep = np.zeros((nx_l, 1))
-            else:
-                desired_sep = sep
-
-            obj = 0
-            for k in range(N):
-                obj += (
-                    (self.agents[i].mpc.x[:, k] - x_pred_ahead[:, k] - desired_sep.T)
-                    @ Q_x_l
-                    @ (
-                        self.agents[i].mpc.x[:, [k]]
-                        - x_pred_ahead[:, [k]]
-                        - desired_sep
-                    )
-                    + self.agents[i].mpc.u[:, k] @ Q_u_l @ self.agents[i].mpc.u[:, [k]]
-                    + w * self.agents[i].mpc.s_ahead[:, [k]]
-                    + w * self.agents[i].mpc.s_behind[:, [k]]
-                )
-            obj += (
-                (self.agents[i].mpc.x[:, N] - x_pred_ahead[:, N] - desired_sep.T)
-                @ Q_x_l
-                @ (self.agents[i].mpc.x[:, [N]] - x_pred_ahead[:, [N]] - desired_sep)
-                + w * self.agents[i].mpc.s_ahead[:, [N]]
-                + w * self.agents[i].mpc.s_behind[:, [N]]
-            )
-            self.agents[i].mpc.mpc_model.setObjective(obj, gp.GRB.MINIMIZE)
+                self.agents[i].mpc.set_x_front(x_pred_front)
+                self.agents[i].mpc.set_x_back(x_pred_back)
 
     def extrapolate_position(self, initial_pos, initial_vel):
         x_pred = np.zeros((nx_l, N + 1))
@@ -197,11 +246,23 @@ class TrackingDecentMldCoordinator(MldAgent):
 # env
 env = MonitorEpisodes(TimeLimit(CarFleet(acc, n, ep_len), max_episode_steps=ep_len))
 
+if DISCRETE_GEARS:
+    mpc_class = LocalMpcGear
+    system = friction_system
+else:
+    mpc_class = LocalMpcMld
+    system = full_system
+
 # coordinator
 local_mpcs: list[MpcMld] = []
 for i in range(n):
     # passing local system
-    local_mpcs.append(LocalMpcMld(system, N))
+    if i == 0:
+        local_mpcs.append(mpc_class(system, N, leader=True))
+    elif i == n - 1:
+        local_mpcs.append(mpc_class(system, N, trailer=True))
+    else:
+        local_mpcs.append(mpc_class(system, N))
 agent = TrackingDecentMldCoordinator(local_mpcs, nx_l, nu_l)
 
 agent.evaluate(env=env, episodes=1, seed=1)
