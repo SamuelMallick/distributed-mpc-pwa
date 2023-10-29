74461e55efe2a318c930fb01729b5f24c00d1036
mallick <sam.mallick.97@gmail.com>
2023-10-29 16:24:28 +0100

    fleet - cent brought up to speed with inhomog

--- examples/ACC_fleet/fleet_cent_mld.py
+++ examples/ACC_fleet/fleet_cent_mld.py
@@ -17,15 +17,17 @@ from dmpcpwa.agents.mld_agent import MldAgent
 from dmpcpwa.mpc.mpc_mld import MpcMld
 from dmpcpwa.mpc.mpc_mld_cent_decup import MpcMldCentDecup
 
-np.random.seed(3)
+np.random.seed(1)
 
 PLOT = True
-SAVE = False
+SAVE = True
 
 n = 3  # num cars
-N = 7  # controller horizon
-COST_2_NORM = True
+N = 10  # controller horizon
+COST_2_NORM = False
 DISCRETE_GEARS = False
+HOMOGENOUS = True
+LEADER_TRAJ = 1  # "1" - constant velocity leader traj. Vehicles start from random ICs. "2" - accelerating leader traj. Vehicles start in perfect platoon.
 
 if len(sys.argv) > 1:
     n = int(sys.argv[1])
@@ -35,11 +37,19 @@ if len(sys.argv) > 3:
     COST_2_NORM = bool(int(sys.argv[3]))
 if len(sys.argv) > 4:
     DISCRETE_GEARS = bool(int(sys.argv[4]))
+if len(sys.argv) > 5:
+    HOMOGENOUS = bool(int(sys.argv[5]))
+if len(sys.argv) > 6:
+    LEADER_TRAJ = int(sys.argv[6])
+
+random_ICs = False
+if LEADER_TRAJ == 1:
+    random_ICs = True
 
 w = 1e4  # slack variable penalty
-ep_len = 200  # length of episode (sim len)
+ep_len = 100  # length of episode (sim len)
 
-acc = ACC(ep_len, N)
+acc = ACC(ep_len, N, leader_traj=LEADER_TRAJ)
 nx_l = acc.nx_l
 nu_l = acc.nu_l
 Q_x_l = acc.Q_x_l
@@ -47,12 +57,11 @@ Q_u_l = acc.Q_u_l
 sep = acc.sep
 d_safe = acc.d_safe
 leader_state = acc.get_leader_state()
-friction_system = acc.get_friction_pwa_system()
 
 
 class MPCMldCent(MpcMldCentDecup):
-    def __init__(self, system: dict, n, N: int) -> None:
-        super().__init__(system, n, N)
+    def __init__(self, systems: list[dict], n, N: int) -> None:
+        super().__init__(systems, n, N)
         self.setup_cost_and_constraints(self.u)
 
     def setup_cost_and_constraints(self, u):
@@ -128,23 +137,27 @@ class MPCMldCent(MpcMldCentDecup):
 
 
 class MpcGearCent(MPCMldCent, MpcMldCentDecup, MpcGear):
-    def __init__(self, system: dict, n: int, N: int) -> None:
-        MpcMldCentDecup.__init__(self, system, n, N)  # use the MpcMld constructor
-        F = block_diag(*([system["F"]] * n))
-        G = np.vstack([system["G"]] * n)
+    def __init__(self, systems: list[dict], n: int, N: int) -> None:
+        MpcMldCentDecup.__init__(self, systems, n, N)  # use the MpcMld constructor
+        F = block_diag(
+            *([systems[0]["F"]] * n)
+        )  # here we are assuming that the F and G are the same for all systems
+        G = np.vstack([systems[0]["G"]] * n)
         self.setup_gears(N, acc, F, G)
         self.setup_cost_and_constraints(self.u_g)
 
 
 class TrackingMldAgent(MldAgent):
     def __init__(self, mpc: MpcMld) -> None:
-        self.run_times = np.zeros((ep_len, 1))
+        self.solve_times = np.zeros((ep_len, 1))
+        self.node_counts = np.zeros((ep_len, 1))
         super().__init__(mpc)
 
     def on_timestep_end(self, env: Env, episode: int, timestep: int) -> None:
         # time step starts from 1, so this will set the cost accurately for the next time-step
         self.mpc.set_leader_traj(leader_state[:, timestep : (timestep + N + 1)])
-        self.run_times[env.step_counter - 1, :] = self.run_time
+        self.solve_times[env.step_counter - 1, :] = self.run_time
+        self.node_counts[env.step_counter - 1, :] = self.node_count
         return super().on_timestep_end(env, episode, timestep)
 
     def on_episode_start(self, env: Env, episode: int) -> None:
@@ -153,15 +166,34 @@ class TrackingMldAgent(MldAgent):
 
 
 # env
-env = MonitorEpisodes(TimeLimit(CarFleet(acc, n, ep_len), max_episode_steps=ep_len))
+env = MonitorEpisodes(
+    TimeLimit(
+        CarFleet(
+            acc,
+            n,
+            ep_len,
+            L2_norm_cost=COST_2_NORM,
+            homogenous=HOMOGENOUS,
+            random_ICs=random_ICs,
+        ),
+        max_episode_steps=ep_len,
+    )
+)
 
 if DISCRETE_GEARS:
-    mpc = MpcGearCent(friction_system, n, N)
+    if HOMOGENOUS:  # by not passing the index all systems are the same
+        systems = [acc.get_friction_pwa_system() for i in range(n)]
+    else:
+        systems = [acc.get_friction_pwa_system(i) for i in range(n)]
+    mpc = MpcGearCent(systems, n, N)
     mpc.set_leader_traj(leader_state[:, 0 : N + 1])
     agent = TrackingMldAgent(mpc)
 else:
     # mld mpc
-    systems = [acc.get_pwa_system(i) for i in range(n)]
+    if HOMOGENOUS:  # by not passing the index all systems are the same
+        systems = [acc.get_pwa_system() for i in range(n)]
+    else:
+        systems = [acc.get_pwa_system(i) for i in range(n)]
     mld_mpc = MPCMldCent(systems, n, N)
     # initialise the cost with the first tracking point
     mld_mpc.set_leader_traj(leader_state[:, 0 : N + 1])
@@ -180,14 +212,14 @@ else:
 
 print(f"Return = {sum(R.squeeze())}")
 print(f"Violations = {env.unwrapped.viol_counter}")
-print(f"Run_times_sum: {sum(agent.run_times)}")
+print(f"Run_times_sum: {sum(agent.solve_times)}")
 
 if PLOT:
     plot_fleet(n, X, U, R, leader_state, violations=env.unwrapped.viol_counter[0])
 
 if SAVE:
     with open(
-        f"cent_n_{n}_N_{N}_Q_{COST_2_NORM}"
+        f"cent_n_{n}_N_{N}_Q_{COST_2_NORM}_DG_{DISCRETE_GEARS}_HOM_{HOMOGENOUS}_LT_{LEADER_TRAJ}"
         + datetime.datetime.now().strftime("%d%H%M%S%f")
         + ".pkl",
         "wb",
@@ -195,4 +227,7 @@ if SAVE:
         pickle.dump(X, file)
         pickle.dump(U, file)
         pickle.dump(R, file)
-        pickle.dump(agent.run_times, file)
+        pickle.dump(agent.solve_times, file)
+        pickle.dump(agent.node_counts, file)
+        pickle.dump(env.unwrapped.viol_counter[0], file)
+        pickle.dump(leader_state, file)
