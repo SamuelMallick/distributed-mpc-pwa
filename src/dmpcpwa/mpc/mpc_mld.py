752a0c5ef52cc61bd4dbabf164109c003c37996c
SamuelMallick <sam.mallick.97@gmail.com>
2024-04-22 13:34:14 +0200

    Added thread limit for MIPS

--- src/dmpcpwa/mpc/mpc_mld.py
+++ src/dmpcpwa/mpc/mpc_mld.py
@@ -15,7 +15,7 @@ logger.addHandler(console_handler)
 class MpcMld:
     """An MPC that converts a PWA mpc problem into a MIP."""
 
-    def __init__(self, system: dict, N: int, verbose=False) -> None:
+    def __init__(self, system: dict, N: int, verbose=False, thread_limit: int | None = None) -> None:
         """Instantiate the mld based mpc. In the constructor pwa system is converted
         to mld and the associated dynamics and constraints are created, along with states
         and control variables.
@@ -37,6 +37,8 @@ class MpcMld:
         mpc_model = gp.Model("mld_mpc")
         mpc_model.setParam("OutputFlag", verbose)
         mpc_model.setParam("Heuristics", 0)
+        if thread_limit is not None:
+            mpc_model.params.threads = thread_limit
         # mpc_model.setParam("MIPStart", 1)  # using warm-starting from previous sol
 
         # Uncomment if you need to differentiate between infeasbile and unbounded
